f93c47b6b4114ac80b5270f42b277cbe6ede7b39
wufuheng@gmail.com <wufuheng@gmail.com>
2014-08-18 11:33:34 -0400

    prob of less than 950000

--- hedgefund.R
+++ hedgefund.R
@@ -1,17 +1,25 @@
 problem1 = function(){
+  
   niter = 1e5 # number of iterations
   below = rep(0,niter) # set up storage
   set.seed(2009)
   for (i in 1:niter)
   {
-    r = rnorm(45,mean=.05/253, sd=.23/sqrt(253)) # generate random numbers
+    # generate random numbers
+    r = rnorm(45, mean=.05/253, sd=.23/sqrt(253))
+    
     logPrice = log(1e6) + cumsum(r)
-    minlogP = min(logPrice) # minimum price over next 45 days
+    
+    # minimum price over next 45 days
+    minlogP = min(logPrice)
+
     below[i] = as.numeric(minlogP < log(950000))
   }
-  plot(r)
-  paste(r)
+  
+  # = 0.50988
+  paste(sum(below)/niter)
 }
 
-debug(problem1)
-undebug(problem1)
+#debug(problem1)
+#undebug(problem1)
+problem1()
